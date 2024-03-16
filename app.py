7cfa5bbb2f1d44fc0e6c05abbf999b958a9044a7
Sophie <sophietanker@gmail.com>
2024-03-16 14:38:47 -0400

    publishing website

--- app.py
+++ app.py
@@ -4,3 +4,23 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.get('/about')
+def about():
+    return render_template('about_us.html')
+
+@app.get('/definitions')
+def roadmap():
+    return render_template('definitions.html')
+
+@app.get('/2000s')
+def year2000():
+    return render_template('2000.html')
+
+@app.get('/2010s')
+def year2010():
+    return render_template('2010.html')
+
+@app.get('/all')
+def year2024():
+    return render_template('all.html')
